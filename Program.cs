f569a4430358b69549854fa4841eb1f2528e1192
hliska-ecpi <hliska@ecpi.edu>
2025-05-20 21:05:16 -0400

    Created a menu-based demo to play HighCard and Blackjack.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    This class displays the list of available games and runs the selected option.
+*/
+
+using Microsoft.Win32.SafeHandles;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ICardGame game = null;
+        string choice, again;
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            Console.Clear();
+            Console.WriteLine("=== Card Game Console! ===");
+            Console.WriteLine("Choose a game to play:");
+            Console.WriteLine("1. High Card");
+            Console.WriteLine("2. Blackjack");
+            Console.WriteLine("0. EXIT");
+            Console.WriteLine("Enter Selection: ");
+
+            choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    game = new HighCard();
+                    break;
+                case "2":
+                    game = new BlackJack();
+                    break;
+                case "0":
+                    playAgain = false;
+                    break;
+                default:
+                    Console.WriteLine("Inalid choice. Press any key to try again...");
+                    Console.ReadKey();
+                    continue;
+            }
+
+            Console.Clear();
+            game.Start();
+            game.DealCards();
+            game.Play();
+            game.ShowResult();
+
+            Console.Write("\n\nWould you like to play another game? (Y/N): ");
+            again = Console.ReadLine();
+            playAgain = again.Trim().ToLower() == "y";
+        }
+
+        Console.WriteLine("Thank you for playing!");
+    }
+}
